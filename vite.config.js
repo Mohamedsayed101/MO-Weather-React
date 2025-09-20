01fd6ed90d1a7ef8a58aabb99aa9362382ba7255
Mohamed Sayed Omar <mo.sayedomar123@gmail.com>
2025-09-21 02:37:53 +0300

    v1.5 update vite config File

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/MO-Weather-React/", // 👈 اسم الـ repo بتاعك على GitHub
+  base: "./",
+  build: {
+    outDir: "dist",
+  },
 });
